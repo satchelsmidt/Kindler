b6a2ee8024c404eed47e5bbc4f13b3ebc19b096f
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-11-14 19:23:25 -0800

    things

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Has technology gone too far?</Text>
+      <Text>Peanut Butter</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
