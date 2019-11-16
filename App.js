f841515efe49e79f4b3eae8df98cb3f45f3f5bcb
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-11-16 13:40:38 -0800

    things

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Google from 'expo-google-app-auth';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Has technology gone too far?</Text>
-      <Text>Peanut Butter</Text>
+      <Text>Deron is Dum</Text>
 
     </View>
   );
+}
+
+async function signInWithGoogleAsync(){
+  try{
+    const result = await Google.logInAsync({
+      androidClientId: '220715676294-6sqt060756ji445a4ru0q10gkteamqbv.apps.googleusercontent.com',
+      iosClientId: '220715676294-o3v7hl5mj6l0rd4ubjvbfia9h02jb6hb.apps.googleusercontent.com',
+      scopes: ['profile', 'email']
+    });
+
+    if(result.type === 'success'){
+      return result.accessToken;
+    }else{
+      return {cancelled: true};
+    }
+  } catch(e){
+    return {error: true};
+  }
 }
 
 const styles = StyleSheet.create({
